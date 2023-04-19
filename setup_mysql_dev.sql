88152986fd7bff6f30cc4766256560adf47718fb
Toby16 <bolajibaasit14@gmail.com>
2023-04-19 15:45:23 -0400

    MySQL setup development | Report: included "FLUSH PRIVILEGES" for script to take effect immediately

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,8 +5,8 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 
--- grant priviledges
+-- grant privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
 
-
+FLUSH PRIVILEGES;
