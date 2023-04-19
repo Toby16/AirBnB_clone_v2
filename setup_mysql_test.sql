4bfad4d1f376ff2f09a3438deb4ae81eee6f28e6
Toby16 <bolajibaasit14@gmail.com>
2023-04-19 15:54:39 -0400

    MySQL setup test | Report: script that prepares a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- create database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+
+-- grant privileges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO "hbnb_test"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
+
+FLUSH PRIVILEGES;
