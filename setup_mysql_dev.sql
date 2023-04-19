30c4b2b85ca9ace5d7552f1dbf10904e2e020614
Toby16 <bolajibaasit14@gmail.com>
2023-04-19 15:43:14 -0400

    MySQL setup development | Report: script that prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+
+-- grant priviledges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
+
+
