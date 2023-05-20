0de2a036f3af3ec297660cfa80cd301f8b326d1c
Toby16 <bolajibaasit14@gmail.com>
2023-05-19 22:43:19 -0400

    Odd or even? | Report: script working fine

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+A python script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Display a greeting message
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_():
+    """
+    Display a message
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text=None):
+    """
+    Display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    if text is not None:
+        return "Python {}".format(text.replace("_", " "))
+    return "Python is cool"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n_(n):
+    """
+    Returns a string indicating whether the provided input is a number.
+    """
+    return "{} is a number".format(str(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template_n(n):
+    """
+    Return template only if n is an integer
+    """
+    return render_template("5-number.html", value=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    """
+    check if n is even or odd
+    and render template according to the value
+    """
+    if n % 2 == 0:
+        value = "is even"
+    else:
+        value = "is odd"
+    str_val = "{} {}".format(n, value)
+    return render_template("6-number_odd_or_even.html", value=str_val)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
